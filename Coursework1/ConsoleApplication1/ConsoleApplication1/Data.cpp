2410eb3c7b5bb5319adcd5ffdea6da7bd21e8671
Aaditya Parashar <aadityaparashar17@gmail.com>
2019-03-31 17:53:47 +0300

    Added some Data class functions

--- Coursework1/ConsoleApplication1/ConsoleApplication1/Data.cpp
+++ Coursework1/ConsoleApplication1/ConsoleApplication1/Data.cpp
@@ -1,1 +1,87 @@
-#include "Data.h"
+#include "Date.h"
+#include <iostream>
+#include <random>
+
+using namespace std;
+Date::Date()
+{
+	time(&Now);
+	struct tm Tm;
+	localtime_s(&Tm, &Now);
+	Day = Tm.tm_mday; // 1...31
+	strcpy_s(Month, MonthNames[Tm.tm_mon]); // 0...11
+	iMonth = Tm.tm_mon + 1;
+	Year = Tm.tm_year + 1900; // current year - 1900
+}
+
+Date::Date(int d, int m, int y)
+{
+	SetYear(y);
+	SetMonth(m);
+	SetDay(d);
+}
+
+Date::Date(const Date& Original)
+{ // copy constructor
+	Day = Original.Day;
+	Year = Original.Year;
+	iMonth = Original.iMonth;
+	strcpy_s(Month, Original.Month);
+}
+
+
+Date::~Date()
+{
+	if (pText)
+		delete pText;
+	//cout << "destructor called\n";
+}
+
+bool Date::IsLeap(int y)
+{ // from https://www.programiz.com/c-programming/examples/leap-year
+	if (y % 4 == 0)
+	{
+		if (y % 100 == 0)
+		{
+			// year is divisible by 400, hence the year is a leap year
+			if (y % 400 == 0)
+				return true;
+			else
+				return false;
+		}
+		else
+			return true;
+	}
+	else
+		return false;
+}
+
+char* Date::ToString() const
+{
+	(const_cast<Date*>(this))->pText = new char[12];
+	sprintf_s(pText, 12, "%02d %s %d", Day, Month, Year);
+	return pText;
+}
+
+void Date::SetMonth(int m)
+{
+	if (m < 1 || m > 12)
+		throw ("Wrong month");
+	strcpy_s(Month, MonthNames[m - 1]);
+	iMonth = m;
+}
+
+int Date::GetMonth(char* pBuf, int nBuf) const
+{
+	if (!pBuf || nBuf < 4)
+		return iMonth;
+	strcpy_s(pBuf, nBuf, Month);
+	return iMonth;
+}
+
+void Date::SetYear(int y)
+{
+	if (y < 0)
+		throw ("Wrong year");
+	Year = y;
+}
--- Coursework1/ConsoleApplication1/ConsoleApplication1/Data.h
+++ Coursework1/ConsoleApplication1/ConsoleApplication1/Data.h
@@ -1,5 +1,31 @@
 #pragma once
-class Data
-{
+#include <list>
+#include "Item.h"
+#include <Map>
+using namespace std;
+class Data {
+public:
+	Data(int n);
+	~Data();
+	void printAll();
+	int countAll();
+	list<Item>* getGroup(char group);
+	bool pringGroup(char group);
+	int countGroup(char group);
+	list<Item>* getSubGroup(char group, int subGroup);
+	bool printSubGroupByNames(char group, int subGroup);
+	bool printSubGroupByDates(char group, int subGroup);
+	int countSubGroup(char group, int subGroup);
+	Item* GetItem(char g, int sg, string n);
+	Item* GetItem(char g, int sg, Date d);
+	bool PrintItem(char g, int sg, string n);
+	bool PrintItem(char g, int sg, Date d);
+	bool RemoveGroup(char g);
+	bool RemoveSubgroup(char g, int sg);
+	bool RemoveItem(char g, int sg, string n);
+	bool RemoveItem(char g, int sg, Date d);
+private:
+	map<char, list<Item>>* dataSet = new map<char, list<Item>>();
+	list<Item> objectList;
+	struct prt_pairs;
 };
-
