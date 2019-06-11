04cebcd96bb473b18409f5d0db4aedb1ef9a7a42
Aaditya Parashar <aadityaparashar17@gmail.com>
2019-06-11 16:16:52 +0300

    Made some small tweaks

--- Coursework2/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ Coursework2/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,9 +1,3 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-// CourseWork2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <windows.h>
 #include <thread>
